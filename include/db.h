64582e1bb65c17b96160507aafd46610c23152bd
nicomarcan <marcantonionicolas@gmail.com>
2016-10-03 12:11:04 -0300

    update db.h

--- include/db.h
+++ include/db.h
@@ -1,13 +1,8 @@
 #include <msg_structs.h>
 #include <stdint.h>
-#define LOSE 0
-#define WIN 1
+
 
 int open_database();
 void insert_highscore(char * name,int score);
 Highscore * get_highscores(int * count);
-void add_id(char * name);
-void update_id(char * name,int type);
-Stat * get_stats(int * count);
-int clear_stats();
 int clear_highscores();
